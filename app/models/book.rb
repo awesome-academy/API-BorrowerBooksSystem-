f3e530e7a3409f202af1b04a4270f7be500feb0d
Phan Minh Huy <phanminhhuy1509@gmail.com>
2021-05-21 17:07:47 +0700

    Setup association for models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  belongs_to :category
+  has_many :requests, dependent: :nullify
+  has_many :book_followers, dependent: :destroy
+  has_many :followers, through: :book_followers,
+            source: :user, dependent: :destroy
 end
